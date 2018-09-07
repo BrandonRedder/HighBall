eabe5a7fbcbe8a4b55627a856acea2a4533af4eb
Richardson, David K <drichardson42@gatech.edu>
2018-09-07 11:52:57 -0400

    Control control code to OOP
    The control algorithm uses an altitude range, as well as thresholds for
    velocity and acceleration in addition to methods to control altitude.
    This lends itself to using a class instead of multiple global variables
    and separate functions.
    
    Updated Control.h and Control.cpp to use OOP styles
    Moved old Control.h and Control.cpp to *.bak temporarily

--- Software/Control Payload/Control Payload 0-1/Payload_Main/Control.cpp
+++ Software/Control Payload/Control Payload 0-1/Payload_Main/Control.cpp
@@ -1,47 +1,57 @@
 #include "Control.h"
 
-// min altitude of 60 kft default
-// max altitude of 80 kft default
-float min_altitude = 60000;
-float max_altitude = 80000;
+// min and max altitude defaults [ft] 
+#define MIN_ALTITUDE 60000
+#define MAX_ALTITUDE 80000
+// min velocity and acceleration defaults
+#define MIN_VELOCITY 0
+#define MIN_ACCEL 0
 
-void set_altitude(float lower_bound, float upper_bound) {
+// default constructor, uses default altitudes, velocity, and acceleration
+altitude_control::altitude_control() {
+  set_altitude_range(MIN_ALTITUDE, MAX_ALTITUDE);
+  set_rates(MIN_VELOCITY, MIN_ACCEL);
+}
+
+// constructor, allows user specified altitudes, velocity and acceleration
+altitude_control::altitude_control(float _min_altitude, float _max_altitude,
+                                   float _min_velocity, float _min_accel) {
+  set_altitude_range(_min_altitude, _max_altitude);
+  set_rates(_min_velocity, _min_accel);
+}
+
+void altitude_control::set_altitude_range(float _min_altitude, float _max_altitude) {
   /* Set minimum and maximum altitude bounds for the altitude control system
    *
    * Inputs
    * ------
-   *  lower_bound: float representing the lowest acceptable altitude
-   *  upper_bound: float representing the highest acceptable altitude
+   *  _min_altitude: float representing the lowest acceptable altitude
+   *  _max_altitude: float representing the highest acceptable altitude
    */
-  min_altitude = lower_bound;
-  max_altitude = upper_bound;
+  set_min_altitude(_min_altitude);
+  set_max_altitude(_max_altitude);
 }
 
-// velocity and acceleration cutoff values
-// allows adjusting the rate of altitude changes
-float min_velocity = 0;
-float min_accel = 0;
-
-void set_rates(float velocity_cutoff, float accel_cutoff) {
+void altitude_control::set_rates(float _min_velocity, float _min_accel) {
   /* Set minimum velocity and acceleration values for the balloon to be
    * considered moving or accelerating.
    *
    * Inputs
    * ------
-   *  velocity_cut: float representing the lowest velocity to be considered
+   *  _min_velocity: float representing the lowest velocity to be considered
    *                moving
-   *  accel_cut: float representing the lowest acceleration to be considered
+   *  _min_accel: float representing the lowest acceleration to be considered
    *             accelerating
    */
-  min_velocity = velocity_cutoff;
-  min_accel = accel_cutoff;
+  set_min_velocity(_min_velocity);
+  set_min_accel(_min_accel);
 }
 
 #define NO_ACTION 0
 #define DROP_BALLAST 1
 #define RELEASE_HELIUM 2
 
-int altitude_control(float alt, float velocity, float accel) {
+int altitude_control::get_action(float alt, float velocity, float accel) {
   /* Determines what action to take to maintain altitude between min_alittude
    * and max_altitude
    *
@@ -58,16 +68,17 @@ int altitude_control(float alt, float velocity, float accel) {
    *  int: 0, 1, or 2 corresponding to no action, drop ballast, or release
    *       helium
    */
-  // TODO: acount for the case when acceleration is close to 0, but will not
-  // currently trigger ballast or helium release
-  if (alt<min_altitude && velocity<=min_velocity && accel<=min_accel) {
+  if (alt<get_min_altitude()
+      && velocity<=get_min_velocity()
+      && accel<=get_min_accel()) {
     /* Below altitude range
      * Not moving upward fast enough
      * Not enough upward acceleration
      */
     return DROP_BALLAST;
-  } else if (alt>max_altitude && velocity>=(0-min_velocity)
-             && accel>=(0-min_accel)) {
+  } else if (alt>get_max_altitude() 
+             && velocity>=(0-get_min_velocity())
+             && accel>=(0-get_min_accel())) {
     /* Above altitude range
      * Not moving downward fast enough
      * Not enough downard acceleration
@@ -76,3 +87,29 @@ int altitude_control(float alt, float velocity, float accel) {
   } // end if
   return NO_ACTION;
 } // end altitude_control
+
+// get functions for each member variable
+float altitude_control::get_min_altitude() {return min_altitude;}
+
+float altitude_control::get_max_altitude() {return max_altitude;}
+
+float altitude_control::get_min_velocity() {return min_velocity;}
+
+float altitude_control::get_min_accel() {return min_accel;}
+
+// set functions for each member variable
+void altitude_control::set_min_altitude(float _min_altitude) {
+  min_altitude = _min_altitude;
+}
+
+void altitude_control::set_max_altitude(float _max_altitude) {
+  max_altitude = _max_altitude;
+}
+
+void altitude_control::set_min_velocity(float _min_velocity) {
+  min_velocity = _min_velocity;
+}
+
+void altitude_control::set_min_accel(float _min_accel) {
+  min_accel = _min_accel;
+}
--- Software/Control Payload/Control Payload 0-1/Payload_Main/Control.h
+++ Software/Control Payload/Control Payload 0-1/Payload_Main/Control.h
@@ -1,13 +1,28 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
-//Constants
+class altitude_control {
+  public:
+    altitude_control();
+    altitude_control(float, float, float, float);
+    void set_altitude_range(float, float);
+    void set_rates(float, float);
+    int get_action(float, float, float);
 
-//Functions
-//Main Setup Function
-void set_altitude(float lower_bound, float upper_bound);
-void set_rates(float velocity_cutoff, float accel_cutoff);
-int altitude_control(float alt, float velocity, float accel);
-// void setup_Control(void);
+    float get_min_altitude();
+    float get_max_altitude();
+    float get_min_velocity();
+    float get_min_accel();
+    void set_min_altitude(float);
+    void set_max_altitude(float);
+    void set_min_velocity(float);
+    void set_min_accel(float);
+
+  private:
+    float min_altitude;
+    float max_altitude;
+    float min_velocity;
+    float min_accel;
+};
 
 #endif /* ifndef CONTROL_H */
